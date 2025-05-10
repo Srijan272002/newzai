07af3ba049feada3d490a84fb4ae9fb6b3d5c96a
Srijan Telang <srijantelang41@gmail.com>
2025-05-11 01:37:50 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://newz-backend.vercel.app/',
+        target: 'http://localhost:3001/',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'https://newz-backend.vercel.app/',
+        target: 'http://localhost:3001/',
         changeOrigin: true,
         ws: true,
       },
